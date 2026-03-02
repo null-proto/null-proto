282c130355c4f8b689638dac96a70995996590ea
null-proto <relayx@duck.com>
2026-03-02 11:25:19 +0530

    decrypted

---
+++ users.nix
@@ -0,0 +1,9 @@
+{
+  profile = {
+    username = "ring1";
+    fullname = "ring1";
+    homeDir = "/dyn/r1ng";
+
+		hash = "$6$cr/HBrSpabEfE3RF$eOXZ/Eo6JO5z22HPqCKP2dVYr5FQceQsxlBLPGq2.uh1etbOssxc4qXXfBXa9qFWzUqLDoLkk0yKQj5RT48Ac/";
+  };
+}
